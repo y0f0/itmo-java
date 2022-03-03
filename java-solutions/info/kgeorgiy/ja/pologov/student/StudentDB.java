effeaa5146696bed846af86a8742f59b088a9308
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2022-03-03 16:28:49 +0300

    Checked by an instructor

--- java-solutions/info/kgeorgiy/ja/pologov/student/StudentDB.java
+++ java-solutions/info/kgeorgiy/ja/pologov/student/StudentDB.java
@@ -15,44 +15,45 @@ public class StudentDB implements StudentQuery {
     private final Comparator<Student> comparator = Comparator
             .comparing(Student::getLastName)
             .thenComparing(Student::getFirstName)
+            // :NOTE: Упростить
             .thenComparing((Student s1, Student s2) -> Integer.compare(s2.getId(), s1.getId()))
             .reversed();
 
-    private <T> List<T> getStudentsField(Collection<Student> students, Function<Student, T> getter) {
+    private <T> List<T> getStudentsField(final Collection<Student> students, final Function<Student, T> getter) {
         return students.stream()
                 .map(getter)
                 .collect(Collectors.toList());
     }
 
     @Override
-    public List<String> getFirstNames(List<Student> students) {
+    public List<String> getFirstNames(final List<Student> students) {
         return getStudentsField(students, Student::getFirstName);
     }
 
     @Override
-    public List<String> getLastNames(List<Student> students) {
+    public List<String> getLastNames(final List<Student> students) {
         return getStudentsField(students, Student::getLastName);
     }
 
     @Override
-    public List<GroupName> getGroups(List<Student> students) {
+    public List<GroupName> getGroups(final List<Student> students) {
         return getStudentsField(students, Student::getGroup);
     }
 
     @Override
-    public List<String> getFullNames(List<Student> students) {
+    public List<String> getFullNames(final List<Student> students) {
         return getStudentsField(students, s -> s.getFirstName() + " " + s.getLastName());
     }
 
     @Override
-    public Set<String> getDistinctFirstNames(List<Student> students) {
+    public Set<String> getDistinctFirstNames(final List<Student> students) {
         return students.stream()
                 .map(Student::getFirstName)
                 .collect(Collectors.toCollection(TreeSet::new));
     }
 
     @Override
-    public String getMaxStudentFirstName(List<Student> students) {
+    public String getMaxStudentFirstName(final List<Student> students) {
         return students.stream()
                 .max(Student::compareTo)
                 .map(Student::getFirstName)
@@ -60,42 +61,43 @@ public class StudentDB implements StudentQuery {
     }
 
     @Override
-    public List<Student> sortStudentsById(Collection<Student> students) {
+    public List<Student> sortStudentsById(final Collection<Student> students) {
         return students.stream()
                 .sorted(Comparator.comparing(Student::getId))
                 .collect(Collectors.toList());
     }
     @Override
-    public List<Student> sortStudentsByName(Collection<Student> students) {
+    public List<Student> sortStudentsByName(final Collection<Student> students) {
         return students.stream()
                 .sorted(comparator)
                 .collect(Collectors.toList());
     }
 
-    private List<Student> findStudentsByField(Collection<Student> students, Predicate<Student> predicate) {
+    private List<Student> findStudentsByField(final Collection<Student> students, final Predicate<Student> predicate) {
         return students.stream()
-                .sorted(comparator)
                 .filter(predicate)
+                .sorted(comparator)
                 .collect(Collectors.toList());
     }
 
+    // :NOTE: Дубли
     @Override
-    public List<Student> findStudentsByFirstName(Collection<Student> students, String name) {
+    public List<Student> findStudentsByFirstName(final Collection<Student> students, final String name) {
         return findStudentsByField(students, student -> student.getFirstName().equals(name));
     }
 
     @Override
-    public List<Student> findStudentsByLastName(Collection<Student> students, String name) {
+    public List<Student> findStudentsByLastName(final Collection<Student> students, final String name) {
         return findStudentsByField(students, student -> student.getLastName().equals(name));
     }
 
     @Override
-    public List<Student> findStudentsByGroup(Collection<Student> students, GroupName group) {
+    public List<Student> findStudentsByGroup(final Collection<Student> students, final GroupName group) {
         return findStudentsByField(students, student -> student.getGroup().equals(group));
     }
 
     @Override
-    public Map<String, String> findStudentNamesByGroup(Collection<Student> students, GroupName group) {
+    public Map<String, String> findStudentNamesByGroup(final Collection<Student> students, final GroupName group) {
         return students.stream()
                 .filter(student -> student.getGroup().equals(group))
                 .collect(Collectors.toMap(Student::getLastName, Student::getFirstName,
