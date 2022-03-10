a3ca28f11353687434dca523b084f8178de6bf27
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2022-03-10 15:46:24 +0300

    Checked by an instructor

--- java-solutions/info/kgeorgiy/ja/pologov/walk/Walk.java
+++ java-solutions/info/kgeorgiy/ja/pologov/walk/Walk.java
@@ -9,7 +9,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Walk {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         if (args == null) {
             System.err.println("Error: arguments are null");
         } else if (args.length < 2) {
@@ -21,68 +21,70 @@ public class Walk {
         } else {
             try {
                 crawlFiles(args[0], args[1]);
-            } catch (Exception e) {
+            } catch (final Exception e) {
                 System.err.println(e.getMessage());
             }
         }
     }
 
-    private static void crawlFiles(String input, String output) throws WalkException {
-        Path inputPath, outputPath;
+    private static void crawlFiles(final String input, final String output) throws WalkException {
+        // :NOTE: Функция
+        final Path inputPath;
         try {
             inputPath = Path.of(input);
-        } catch (InvalidPathException e) {
+        } catch (final InvalidPathException e) {
             throw new WalkException("Error: invalid input file path");
         }
+
+        final Path outputPath;
         try {
             outputPath = Path.of(output);
-        } catch (InvalidPathException e) {
+        } catch (final InvalidPathException e) {
             throw new WalkException("Error: invalid output file path");
         }
-        if (Files.notExists(inputPath)) {
-            throw new WalkException("Error: input file not exists");
-        }
+
+//        if (Files.notExists(inputPath)) {
+//            throw new WalkException("Error: input file not exists");
+//        }
+        // :NOTE: getParent x3
         if (outputPath.getParent() != null && Files.notExists(outputPath.getParent()))  {
             try {
                 Files.createDirectories(outputPath.getParent());
-            } catch (IOException e) {
+            } catch (final IOException e) {
+                // :NOTE: Не повод
                 throw new WalkException("Error: can't create directories before output file");
             }
         }
+
+        // :NOTE: Кодировки
         try (
-                BufferedReader reader = Files.newBufferedReader(inputPath);
-                BufferedWriter writer = Files.newBufferedWriter(outputPath)
+                final BufferedReader reader = Files.newBufferedReader(inputPath);
+                final BufferedWriter writer = Files.newBufferedWriter(outputPath)
         ) {
             String pathStr;
             while ((pathStr = reader.readLine()) != null) {
-                MessageDigest digest = MessageDigest.getInstance("SHA-1");
-                Path path = null;
-                try {
-                    path = Path.of(pathStr);
-                } catch (InvalidPathException e) {
+                // :NOTE: Переиспользовать
+                final MessageDigest digest = MessageDigest.getInstance("SHA-1");
+                try (final InputStream inputStream = new DigestInputStream(Files.newInputStream(Path.of(pathStr)), digest)) {
+                    // :NOTE: Переиспользовать
+                    final byte[] bytes = new byte[1024];
+                    while (inputStream.read(bytes) > 0);
+                    //:fixed: Exception
+                    writeHash(writer, pathStr, bytesToHexString(digest.digest()));
+                } catch (final InvalidPathException | IOException e) {
                     writeHash(writer, pathStr, "0000000000000000000000000000000000000000");
                 }
-
-                if (path != null) {
-                    try (InputStream inputStream = new DigestInputStream(Files.newInputStream(path), digest)) {
-                        byte[] bytes = new byte[1024];
-                        while (inputStream.read(bytes) > 0);
-                        //:fixed: Exception
-                        writeHash(writer, pathStr, bytesToHexString(digest.digest()));
-                    } catch (IOException e) {
-                        writeHash(writer, pathStr, "0000000000000000000000000000000000000000");
-                    }
-                }
             }
-        } catch (WalkException | SecurityException | IOException | NoSuchAlgorithmException e) {
+        } catch (final WalkException | SecurityException | IOException | NoSuchAlgorithmException e) {
+            // :NOTE: Что и когда произошло
             throw new WalkException("Error: can't open input/output file");
         }
     }
 
-    public static String bytesToHexString(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (byte b : bytes) {
-            int value = b & 0xFF;
+    public static String bytesToHexString(final byte[] bytes) {
+        final StringBuilder sb = new StringBuilder();
+        for (final byte b : bytes) {
+            final int value = b & 0xFF;
             if (value < 16) {
                 sb.append("0");
             }
@@ -91,11 +93,11 @@ public class Walk {
         return sb.toString();
     }
 
-    private static void writeHash(BufferedWriter writer, String path, String hash) throws WalkException {
+    private static void writeHash(final BufferedWriter writer, final String path, final String hash) throws WalkException {
         try {
             writer.write(hash + " " + path);
             writer.newLine();
-        } catch (IOException e) {
+        } catch (final IOException e) {
             // :fixed: Описать, что случилось
             throw new WalkException("can't write output file");
         }
